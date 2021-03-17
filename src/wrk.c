bf8133e1380bed76f81de7ecdc2d25a2ae21e020
AmpereTravis <48371668+AmpereTravis@users.noreply.github.com>
2021-03-16 17:19:14 -0700

    Update parse_args to use explicitly signed char to avoid argparse bugs.
    Without this, argparse silently fails on aarch64.

--- src/wrk.c
+++ src/wrk.c
@@ -743,7 +743,7 @@ static struct option longopts[] = {
 };
 
 static int parse_args(struct config *cfg, char **url, struct http_parser_url *parts, char **headers, int argc, char **argv) {
-    char c, **header = headers;
+    signed char c, **header = headers;
 
     memset(cfg, 0, sizeof(struct config));
     cfg->threads     = 2;
